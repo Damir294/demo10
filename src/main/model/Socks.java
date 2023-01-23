9c9c14998d46a67576180c2b9f446d9f8e4b0374
Damir294 <117120261+Damir294@users.noreply.github.com>
2023-01-24 00:13:58 +0400

    курсовая 3

---
+++ src/main/model/Socks.java
@@ -0,0 +1,24 @@
+package src.main.model;
+
+
+
+import lombok.Data;
+
+
+public class Socks {
+
+    private int cottonPart;
+    private SocksColor socksColor;
+    private SocksSize socksSize;
+    private boolean inStock;
+
+    public Socks(int cottonPart, SocksColor socksColor, SocksSize socksSize) {
+        if (cottonPart > 0 && cottonPart <= 100) {
+            this.cottonPart = cottonPart;
+        } else {
+            throw new IllegalArgumentException("% содержания хлопка указан как " + cottonPart + "%");
+        }
+        this.socksColor = socksColor;
+        this.socksSize = socksSize;
+    }
+}
---
+++ src/main/model/SocksColor.java
@@ -0,0 +1,16 @@
+package src.main.model;
+
+public enum SocksColor {
+    BLACK ("черный"),
+    WHITE ("белый"),
+    RED ("красный"),
+    YELLOW ("желтый"),
+    BLUE ("синий"),
+    BROWN ("коричневый");
+
+    private final String color;
+
+    SocksColor(String color) {
+        this.color = color;
+    }
+}
